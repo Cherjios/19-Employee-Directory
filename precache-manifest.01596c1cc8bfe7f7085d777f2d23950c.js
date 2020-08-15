550de024a87b2b74276ae6a6bbd80724784a8cb1
cherjios <slopez0286@gmail.com>
2020-08-14 22:19:16 -0700

    Updates

---
+++ precache-manifest.01596c1cc8bfe7f7085d777f2d23950c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd5fde0b3a50660d2db9eb89240baba3",
+    "url": "/19-Employee-Directory/index.html"
+  },
+  {
+    "revision": "ee3259a98d5c4b293550",
+    "url": "/19-Employee-Directory/static/css/main.1f98f727.chunk.css"
+  },
+  {
+    "revision": "f09dda5ffc1510bc5817",
+    "url": "/19-Employee-Directory/static/js/2.c6a5fc8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/19-Employee-Directory/static/js/2.c6a5fc8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee3259a98d5c4b293550",
+    "url": "/19-Employee-Directory/static/js/main.fe19c3fd.chunk.js"
+  },
+  {
+    "revision": "62577ba2b4efe76cbba0",
+    "url": "/19-Employee-Directory/static/js/runtime-main.a8f7473a.js"
+  }
+]);
